944c141d7af70b51807b3042a7e429163023240c
Adam O'Neil <adamosoftware@gmail.com>
2019-12-20 18:15:51 -0500

    Base version 1.0.4 RowExistsAsync

---
+++ Dapper.CX.Base/Extensions/ConnectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Threading.Tasks;
+
+namespace Dapper.CX.Extensions
+{
+    public static class ConnectionExtensions
+    {
+        public static async Task<bool> RowExistsAsync(this IDbConnection connection, string fromWhere, object parameters = null, IDbTransaction transaction = null)
+        {
+            return ((await connection.QueryFirstOrDefaultAsync<int?>($"SELECT 1 FROM {fromWhere}", parameters, transaction) ?? 0) == 1);
+        }
+    }
+}
