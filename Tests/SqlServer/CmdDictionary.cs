87f1be9af912040546b8402a0fe7aec3ad4a8114
Adam O'Neil <adamosoftware@gmail.com>
2019-12-20 17:47:37 -0500

    SqlServer version 1.0.3 with SqlServerCmd

---
+++ Tests/SqlServer/CmdDictionary.cs
@@ -0,0 +1,60 @@
+﻿using Dapper.CX.SqlServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Extensions;
+
+namespace Tests.SqlServer
+{
+    [TestClass]
+    public class CmdDictionary
+    {
+        [TestMethod]
+        public void SqlServerInsertCommand()
+        {
+            var ins = new SqlServerCmd("dbo.Table1", "Id")
+            {
+                { "FirstName", "Adam" },
+                { "LastName", "O'Neil" }
+            };
+
+            var cmd = ins.GetInsertStatement();
+            Assert.IsTrue(cmd.ReplaceWhitespace().Equals(
+                @"INSERT INTO [dbo].[Table1] (
+                    [FirstName], [LastName]
+                ) VALUES (
+                    @FirstName, @LastName
+                ); SELECT SCOPE_IDENTITY();".ReplaceWhitespace()));
+        }
+
+        [TestMethod]
+        public void SqlServerUpdateCommand()
+        {
+            var upd = new SqlServerCmd("dbo.Table1", "Id")
+            {
+                { "FirstName", "Adam" },
+                { "LastName", "O'Neil" }
+            };
+
+            var cmd = upd.GetUpdateStatement();
+            Assert.IsTrue(cmd.ReplaceWhitespace().Equals(
+                @"UPDATE [dbo].[Table1] SET
+                    [FirstName]=@FirstName, [LastName]=@LastName
+                WHERE [Id]=@Id".ReplaceWhitespace()));
+        }
+
+        [TestMethod]
+        public void SqlServerUpdateCommandWithKeyColumns()
+        {
+            var upd = new SqlServerCmd("dbo.Table1", "Id")
+            {
+                { "FirstName", "Adam" },
+                { "LastName", "O'Neil" }
+            };
+
+            var cmd = upd.GetUpdateStatement();
+            Assert.IsTrue(cmd.ReplaceWhitespace().Equals(
+                @"UPDATE [dbo].[Table1] SET
+                    [FirstName]=@FirstName, [LastName]=@LastName
+                WHERE [Id]=@Id".ReplaceWhitespace()));
+        }
+    }
+}
