2576f35226e626c65cc8555ea48f88358db4899b
Adam O'Neil <adamosoftware@gmail.com>
2020-01-06 16:03:04 -0500

    version 1.0.2 namespace change in extension methods to resolve ambiguity

--- Dapper.CX.SqlServer/Extensions/SqlServerLongCrud.cs
+++ Dapper.CX.SqlServer/Extensions/SqlServerLongCrud.cs
@@ -4,7 +4,7 @@ using System;
 using System.Data;
 using System.Threading.Tasks;
 
-namespace Dapper.CX.SqlServer.Extensions
+namespace Dapper.CX.SqlServer.Extensions.Long
 {
     public static class SqlServerLongCrud
     {
