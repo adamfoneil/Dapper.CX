ad187a5d079be517331f54f9ffc8e53d3f0f56ec
Adam ONeil <adamosoftware@gmail.com>
2020-01-07 14:29:52 -0500

    version 1.0.9 added Microsoft.Data.SqlClient

--- Dapper.CX.SqlServer/SqlServerCmd.cs
+++ Dapper.CX.SqlServer/SqlServerCmd.cs
@@ -1,8 +1,8 @@
 ﻿using Dapper.CX.Abstract;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
