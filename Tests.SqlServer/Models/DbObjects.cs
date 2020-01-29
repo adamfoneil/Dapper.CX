29daddeb3fe07b1a311675615cfb3edf7269c7d2
Adam ONeil <adamosoftware@gmail.com>
2020-01-29 13:50:42 -0500

    more dependency overlap fixed, removed Tests.Models project

---
+++ Tests.SqlServer/Models/DbObjects.cs
@@ -0,0 +1,33 @@
+﻿using SqlServer.LocalDb.Models;
+using System.Collections.Generic;
+
+namespace Tests.Models
+{
+    public static class DbObjects
+    {
+        public static IEnumerable<InitializeStatement> CreateObjects()
+        {
+            yield return new InitializeStatement(
+                "dbo.Employee",
+                "DROP TABLE %obj%",
+                @"CREATE TABLE %obj% (
+                    [FirstName] nvarchar(50) NOT NULL,
+                    [LastName] nvarchar(50) NOT NULL,
+                    [HireDate] date NULL,
+                    [TermDate] date NULL,
+                    [IsExempt] bit NOT NULL,
+                    [Timestamp] datetime NULL,
+                    [Id] int identity(1, 1) PRIMARY KEY
+                )");
+
+            yield return new InitializeStatement(
+                "dbo.SomethingElse", "DROP TABLE %obj%",
+                @"CREATE TABLE %obj% (
+                    [EmployeeId] int NOT NULL,
+                    [Balance] decimal NULL,
+                    [Whatever] nvarchar(50) NULL,
+                    [Id] int identity(1,1) PRIMARY KEY
+                )");
+        }
+    }
+}
---
+++ Tests.SqlServer/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using Dapper.CX.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace Tests.Models
+{
+    [Identity(nameof(Id))]
+    public class Employee
+    {
+        [PrimaryKey]
+        public string FirstName { get; set; }
+        [PrimaryKey]
+        public string LastName { get; set; }
+        public DateTime? HireDate { get; set; }
+        public DateTime? TermDate { get; set; }
+        public bool IsExempt { get; set; }
+        public DateTime? Timestamp { get; set; }
+        public int Id { get; set; }
+
+        public IEnumerable<string> Something { get; set; }
+        public IEnumerable<DateTime> SomethingElse { get; set; }
+    }
+}
