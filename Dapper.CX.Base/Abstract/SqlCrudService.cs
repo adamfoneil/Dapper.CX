b92fa5e5431759d376868c7f1695895af84ae39b
Adam ONeil <adamosoftware@gmail.com>
2020-02-14 09:18:06 -0500

    started on #8

---
+++ Dapper.CX.Base/Abstract/SqlCrudService.cs
@@ -0,0 +1,44 @@
+﻿using AO.DbSchema.Enums;
+using Dapper.CX.Classes;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Dapper.CX.Abstract
+{
+    public abstract class SqlCrudService<TIdentity>
+    {
+        private readonly SqlCrudProvider<TIdentity> _crudProvider;
+
+        public SqlCrudService(SqlCrudProvider<TIdentity> crudProvider)
+        {
+            _crudProvider = crudProvider;
+        }
+
+        protected abstract IDbConnection GetConnection();
+
+        public async Task<TIdentity> SaveAsync<TModel>(TModel model, ChangeTracker<TModel> changeTracker = null, Action<TModel, SaveAction> onSave = null)
+        {
+            using (var cn = GetConnection())
+            {
+                return await _crudProvider.SaveAsync(cn, model, changeTracker, onSave);
+            }
+        }
+
+        public async Task<TIdentity> MergeAsync<TModel>(TModel model, ChangeTracker<TModel> changeTracker = null, Action<TModel, SaveAction> onSave = null)
+        {
+            using (var cn = GetConnection())
+            {
+                return await _crudProvider.MergeAsync(cn, model, changeTracker, onSave);
+            }
+        }
+
+        public async Task DeleteAsync(TIdentity id)
+        {
+            using (var cn = GetConnection())
+            {
+                await _crudProvider.DeleteAsync<TIdentity>(cn, id);
+            }
+        }
+    }
+}
