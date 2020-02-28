81184ca0839188b2e599c9870382ca4f0de296e6
Adam O'Neil <adamosoftware@gmail.com>
2020-02-28 16:36:44 -0500

    SqlServer version 1.0.20 forgot "this" on ext methods

--- Dapper.CX.SqlServer/Extensions/SqlServerIntCrud_Compatibility.cs
+++ Dapper.CX.SqlServer/Extensions/SqlServerIntCrud_Compatibility.cs
@@ -7,13 +7,13 @@ namespace Dapper.CX.SqlServer.Extensions.Int
 {
     public static partial class SqlServerIntCrud
     {
-        public static async Task<int> SaveAsync<TModel>(IDbConnection connection, TModel @object, params string[] columnNames)
+        public static async Task<int> SaveAsync<TModel>(this IDbConnection connection, TModel @object, params string[] columnNames)
         {
             var provider = new SqlServerIntCrudProvider();
             return await provider.SaveAsync(connection, @object, columnNames);
         }
 
-        public static async Task UpdateAsync<TModel>(IDbConnection connection, TModel @object, params Expression<Func<TModel, object>>[] setColumns)
+        public static async Task UpdateAsync<TModel>(this IDbConnection connection, TModel @object, params Expression<Func<TModel, object>>[] setColumns)
         {
             var provider = new SqlServerIntCrudProvider();
             await provider.UpdateAsync(connection, @object, setColumns);
--- Dapper.CX.SqlServer/Extensions/SqlServerLongCrud_Compatibility.cs
+++ Dapper.CX.SqlServer/Extensions/SqlServerLongCrud_Compatibility.cs
@@ -7,13 +7,13 @@ namespace Dapper.CX.SqlServer.Extensions.Long
 {
     public static partial class SqlServerLongCrud
     {
-        public static async Task<long> SaveAsync<TModel>(IDbConnection connection, TModel @object, params string[] columnNames)
+        public static async Task<long> SaveAsync<TModel>(this IDbConnection connection, TModel @object, params string[] columnNames)
         {
             var provider = new SqlServerIntCrudProvider();
             return await provider.SaveAsync(connection, @object, columnNames);
         }
 
-        public static async Task UpdateAsync<TModel>(IDbConnection connection, TModel @object, params Expression<Func<TModel, object>>[] setColumns)
+        public static async Task UpdateAsync<TModel>(this IDbConnection connection, TModel @object, params Expression<Func<TModel, object>>[] setColumns)
         {
             var provider = new SqlServerIntCrudProvider();
             await provider.UpdateAsync(connection, @object, setColumns);
