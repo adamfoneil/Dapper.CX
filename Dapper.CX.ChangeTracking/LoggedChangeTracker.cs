031f947523caa05c0582bc9d3f668fde42c6288b
Adam ONeil <adamosoftware@gmail.com>
2020-04-21 12:46:36 -0400

    added ChangeTracking project

---
+++ Dapper.CX.ChangeTracking/LoggedChangeTracker.cs
@@ -0,0 +1,19 @@
+﻿using Dapper.CX.Interfaces;
+using System;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Dapper.CX.Classes
+{
+    public class LoggedChangeTracker<TModel> : ChangeTracker<TModel>, IDbSaveable
+    {
+        public LoggedChangeTracker(TModel @object) : base(@object)
+        {
+        }
+
+        public Task SaveAsync(IDbConnection connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dapper.CX.ChangeTracking/Models/ChangeTrackerTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.CX.ChangeTracking.Models
+{
+    public class ChangeTrackerTable
+    {
+
+    }
+}
