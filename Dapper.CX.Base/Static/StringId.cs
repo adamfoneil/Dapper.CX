f6993b815223725cd63237f3938fad17660d9541
Adam O'Neil <adamosoftware@gmail.com>
2020-05-06 08:15:10 -0400

    max attempts arg

--- Dapper.CX.Base/Static/StringId.cs
+++ Dapper.CX.Base/Static/StringId.cs
@@ -10,20 +10,30 @@ namespace Dapper.CX.Static
         // idea from https://scottlilly.com/create-better-random-numbers-in-c/
         private static readonly RNGCryptoServiceProvider _generator = new RNGCryptoServiceProvider();
 
-        public static string New(int length)
+        public static string New(int length, int maxAttempts = -1)
+        {
+            int attempts = 0;
+            return New(length, ref attempts, maxAttempts);
+        }
+
+        public static string New(int length, ref int attempts, int maxAttempts = -1)
         {
             var sourceBytes = Encoding.ASCII.GetBytes("abcdefghijklmnopqrstuvwxyz0123456789");
 
             string result = null;
+            attempts = 0;
 
             do
             {
+                attempts++;
+
                 // the random bytes generated won't necessarily be characters I allow, 
                 // so I generate more than I need, and filter for the allowed
                 byte[] randomBytes = new byte[length * 10];
                 _generator.GetNonZeroBytes(randomBytes);
                 var randomAllowedBytes = randomBytes.Where(b => sourceBytes.Contains(b)).Take(length);
                 result = new string(randomAllowedBytes.Select(b => (char)b).ToArray());
+                if (maxAttempts > 0 && attempts > maxAttempts) throw new Exception($"Couldn't generate desired random characters after {maxAttempts}.");
             } while (result.Length < length);
 
             // you're not certain to get enough good characters to reach the desired result length,
--- Tests.Base/StringIdTests.cs
+++ Tests.Base/StringIdTests.cs
@@ -15,16 +15,21 @@ namespace Tests.Base
             List<string> dups = new List<string>();
 
             const int count = 1_000_000;
-            
+
+            int attempts = 0;
+            int maxAttempts = 0;
+
             for (int i = 0; i < count; i++)
-            {
-                string id = StringId.New(9);
+            {                
+                string id = StringId.New(9, ref attempts);
                 if (!ids.Add(id)) dups.Add(id);
+                if (attempts > maxAttempts) maxAttempts = attempts;
             }            
 
             Assert.IsTrue(ids.Count == count);
 
             // no dups in a million -- good enough for now!
+            // seeing a maxAttempts of 7
         }
     }
 }
