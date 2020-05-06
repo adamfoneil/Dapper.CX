78869c39e6eb56602f24c439734cac77ca0c1806
Adam O'Neil <adamosoftware@gmail.com>
2020-05-05 23:16:44 -0400

    started on StringId.New method #14

---
+++ Dapper.CX.Base/Static/StringId.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper.CX.Static
+{
+    public static class StringId
+    {
+        public static string New(int length)
+        {
+            var sourceChars = "abcdefghijklmnopqrstuvwxyz01234567890.-".ToCharArray();
+
+            int nonConsecutiveFromRight = 2;
+            HashSet<int> nonConsecutiveIndexes = new HashSet<int>();
+            for (int i = sourceChars.Length - nonConsecutiveFromRight; i < sourceChars.Length; i++) nonConsecutiveIndexes.Add(i);
+
+            StringBuilder sb = new StringBuilder();
+                        
+            var rnd = new Random();
+            int index = 0;
+            int priorIndex = 0;
+            var maximums = Enumerable.Range(1, 6).Select(i => i * 8).ToArray();
+            while (sb.Length < length)
+            {                
+                int max = maximums[rnd.Next(maximums.Length)];
+                index += rnd.Next(max);
+                if (index > sourceChars.Length - 1) index %= sourceChars.Length;
+
+                if (nonConsecutiveIndexes.Contains(index))
+                {
+                    while (index == priorIndex)
+                    {
+                        index += rnd.Next(max);
+                        if (index > sourceChars.Length - 1) index %= sourceChars.Length;
+                    }
+                }
+                
+                sb.Append(sourceChars[index]);
+                priorIndex = index;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Tests.Base/StringIdTests.cs
@@ -0,0 +1,32 @@
+﻿using Dapper.CX.Static;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tests.Base
+{
+    [TestClass]
+    public class StringIdTests
+    {
+        [TestMethod]
+        public void GenerateStringIds()
+        {
+            HashSet<string> ids = new HashSet<string>();
+            List<string> dups = new List<string>();
+
+            const int count = 1_000_000;
+            
+            for (int i = 0; i < count; i++)
+            {
+                string id = StringId.New(12);
+                if (!ids.Add(id)) dups.Add(id);
+            }
+
+            var dupGrp = dups.GroupBy(s => s);
+
+            Assert.IsTrue(ids.Count == count);
+
+            // dup rate is about ‭0.00025‬
+        }
+    }
+}
