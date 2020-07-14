fecab310ba1d8858d8c08ceae76203a38e32f8d6
Adam O'Neil <adamosoftware@gmail.com>
2020-07-14 10:16:51 -0400

    added WorkspaceUser

---
+++ SampleApp.Data/WorkspaceUser.cs
@@ -0,0 +1,17 @@
+﻿using AO.Models;
+using Models.Conventions;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleApp.Data
+{
+    public class WorkspaceUser : BaseTable
+    {
+        [Key]
+        [References(typeof(Workspace))]
+        public int WorkspaceId { get; set; }
+
+        [Key]
+        [References(typeof(UserProfile))]
+        public int UserId { get; set; }
+    }
+}
