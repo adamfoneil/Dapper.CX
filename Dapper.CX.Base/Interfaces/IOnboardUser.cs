2ebd5d914c954b1452f7d19653d45724094ee873
Adam O'Neil <adamosoftware@gmail.com>
2021-01-15 12:52:26 -0500

    SqlServer v1.3.26 #40 in progress

--- Dapper.CX.Base/Interfaces/IOnboardUser.cs
+++ Dapper.CX.Base/Interfaces/IOnboardUser.cs
@@ -1,13 +1,15 @@
 ﻿using AO.Models.Interfaces;
+using System.Collections.Generic;
+using System.Security.Claims;
 
 namespace Dapper.CX.Interfaces
 {
-    public interface IGetUser<T> where T : IUserBase
+    public interface IOnboardUser<T> where T : IUserBase
     {
         /// <summary>
         /// Request-scope access to user.
         /// Synchronous because it's used during Startup.ConfigureServices which is not async
         /// </summary>
-        T Get(string userName);
+        T Get(string userName, IEnumerable<Claim> claims);
     }
 }
